2e759170286d924c6598c09ec7d7d101abaee76e
Shawn Collinge <shawnc6@cs.washington.edu>
2024-08-11 12:53:45 -0700

    Update github pages configs

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as dotenv from 'dotenv';
+dotenv.config();
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        outDir: 'dist'
+    },
+    server: {
+        open: true,
+        port: 5173,
+        host: process.env.VITE_HOST || 'localhost'
+    }
+});
