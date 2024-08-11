3ef80f13210df0b4c316584d87644e6a60de5e56
Shawn Collinge <shawnc6@cs.washington.edu>
2024-08-11 12:58:46 -0700

    Add homepage

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
   server: {
     open: true,
     port: 5173,
