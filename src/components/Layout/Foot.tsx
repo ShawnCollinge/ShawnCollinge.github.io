866ce90af4f902d02e93d6e76e7176c38e570e90
Shawn Collinge <shawnc6@cs.washington.edu>
2024-08-11 21:27:56 -0700

    Update footer

--- src/components/Layout/Foot.tsx
+++ src/components/Layout/Foot.tsx
@@ -9,7 +9,7 @@ import {
   useColorModeValue,
   VisuallyHidden,
 } from '@chakra-ui/react'
-import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import { ReactNode } from 'react'
 
 const SocialButton = ({
@@ -24,16 +24,18 @@ const SocialButton = ({
   return (
     <chakra.button
       bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
+      rounded='full'
       w={8}
       h={8}
-      cursor={'pointer'}
-      as={'a'}
+      cursor='pointer'
+      as='a'
       href={href}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
+      display='inline-flex'
+      alignItems='center'
+      justifyContent='center'
+      target="_blank"  
+      rel="noopener noreferrer"
+      transition='background 0.3s ease'
       _hover={{
         bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
       }}>
@@ -56,16 +58,13 @@ export default function Footer() {
         spacing={4}
         justify={{ base: 'center', md: 'space-between' }}
         align={{ base: 'center', md: 'center' }}>
-        <Text>© 2022 Chakra Templates. All rights reserved</Text>
-        <Stack direction={'row'} spacing={6}>
-          <SocialButton label={'Twitter'} href={'#'}>
-            <FaTwitter />
+        <Text>© 2024 Shawn Collinge</Text>
+        <Stack direction='row' spacing={6}>
+          <SocialButton label='Github' href='http://github.com/ShawnCollinge'>
+            <FaGithub />
           </SocialButton>
-          <SocialButton label={'YouTube'} href={'#'}>
-            <FaYoutube />
-          </SocialButton>
-          <SocialButton label={'Instagram'} href={'#'}>
-            <FaInstagram />
+          <SocialButton label='YouTube' href='https://www.linkedin.com/in/s-collinge/'>
+            <FaLinkedin />
           </SocialButton>
         </Stack>
       </Container>
